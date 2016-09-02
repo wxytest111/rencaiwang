f5b21becd183f2a5103df02ba26478a26cc243b7
xinyu <30540047@qq.com>
2016-09-03 07:16:38 +0800

    add regions

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,5 +10,7 @@ Rails.application.config.assets.version = '1.0'
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 Rails.application.config.assets.precompile += [
     'categories.js',
-    'categories.css'
+    'categories.css',
+    'regions.js',
+    'regions.css'
 ]
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160902141625) do
+ActiveRecord::Schema.define(version: 20160902231141) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "title",      limit: 255
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+  end
+
+  create_table "regions", force: :cascade do |t|
+    t.string   "name",       limit: 255
     t.datetime "created_at",             null: false
     t.datetime "updated_at",             null: false
   end
