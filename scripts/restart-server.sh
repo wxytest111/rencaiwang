4f9105f69d147f3ed67383a18063af39c075aeb0
xinyu <30540047@qq.com>
2017-01-07 22:11:34 +0800

    deploy to 202

--- scripts/restart-server.sh
+++ scripts/restart-server.sh
@@ -9,7 +9,7 @@ if [ -f /var/run/rencaiwang.pid ]; then
   pid=`cat /var/run/rencaiwang.pid`
   echo $pid
     if [ -d /proc/$pid ]; then
-      ps -ef | grep unicorn | grep -v grep | cut -c 9-15 | xargs kill -9
+      ps -ef | grep rencaiwang | grep -v grep | cut -c 9-15 | xargs kill -9
     fi
 fi
 
